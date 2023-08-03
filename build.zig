0da8bd6ffb495999d54309cd88a73275203f5041
Ali Chraghi <alichraghi@proton.me>
2023-08-03 08:08:46 +0330

    update to latest Zig API

--- build.zig
+++ build.zig
@@ -6,12 +6,11 @@ pub fn build(b: *std.Build) void {
 
     const lib = b.addStaticLibrary(.{
         .name = "direct3d-headers",
-        .root_source_file = .{ .path = "stub.c" },
         .target = target,
         .optimize = optimize,
     });
     if (lib.target.toTarget().cpu.arch == .x86_64) {
-        lib.addLibraryPath("x86_64");
+        lib.addLibraryPath(.{ .path = "x86_64" });
         lib.linkSystemLibraryName("dxguid");
         lib.linkSystemLibraryName("dxgi");
     }
@@ -23,7 +22,7 @@ pub fn build(b: *std.Build) void {
 // TODO(build-system): this is needed because lib2.linkLibrary(lib)
 // will not add the library path transitively to lib3.linkLibrary(lib2)
 pub fn addLibraryPath(step: *std.build.CompileStep) void {
-    step.addLibraryPath(sdkPath("/x86_64"));
+    step.addLibraryPath(.{ .path = sdkPath("/x86_64") });
 }
 
 fn sdkPath(comptime suffix: []const u8) []const u8 {
