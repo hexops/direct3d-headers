9417c58f11628b7072f0c63b687282de83592891
Stephen Gutekanst <stephen@hexops.com>
2024-06-02 12:26:31 -0700

    update to Zig 2024.05
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- build.zig
+++ build.zig
@@ -11,23 +11,23 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.addWriteFiles().add("empty.c", ""),
     });
     if (target.result.cpu.arch == .x86_64 and target.result.os.tag == .windows) {
-        lib.addLibraryPath(.{ .path = "x86_64" });
+        lib.addLibraryPath(b.path("x86_64"));
         lib.linkSystemLibrary("dxguid");
         lib.linkSystemLibrary("dxgi");
     }
     b.installArtifact(lib);
 
-    lib.installHeadersDirectory("include", ".");
+    lib.installHeadersDirectory(b.path("include"), ".", .{});
 }
 
 // TODO(build-system): this is needed because lib2.linkLibrary(lib)
 // will not add the library path transitively to lib3.linkLibrary(lib2)
 pub fn addLibraryPath(step: *std.Build.Step.Compile) void {
-    step.addLibraryPath(.{ .path = sdkPath("/x86_64") });
+    step.addLibraryPath(.{ .cwd_relative = sdkPath("/x86_64") });
 }
 
 pub fn addLibraryPathToModule(module: *std.Build.Module) void {
-    module.addLibraryPath(.{ .path = sdkPath("/x86_64") });
+    module.addLibraryPath(.{ .cwd_relative = sdkPath("/x86_64") });
 }
 
 fn sdkPath(comptime suffix: []const u8) []const u8 {
